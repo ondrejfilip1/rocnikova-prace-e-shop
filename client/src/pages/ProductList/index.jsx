86f962f435f820043aede1c13f55137de1f3fe51
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2024-12-09 18:47:13 +0100

    Další fix hledání, component pro chybu 404

--- client/src/pages/ProductList/index.jsx
+++ client/src/pages/ProductList/index.jsx
@@ -20,6 +20,8 @@ import {
 
 import { useLocation } from "react-router-dom";
 
+import NotFound from "@/components/NotFound";
+
 export default function ProductList() {
   const [products, setProducts] = useState();
   const [isLoaded, setLoaded] = useState(false);
@@ -36,9 +38,9 @@ export default function ProductList() {
       setLoaded(true);
     }
   };
-
+  
+  const query = new URLSearchParams(location.search);
   useEffect(() => {
-    const query = new URLSearchParams(location.search);
     const queryParam = query.get("search");
     if (queryParam) {
       setSearchQuery(query.get("search"));
@@ -46,7 +48,7 @@ export default function ProductList() {
     } else {
       load("");
     }
-  }, []);
+  }, [query.get("search")]);
 
   useEffect(() => {
     load(searchQuery);
@@ -94,10 +96,7 @@ export default function ProductList() {
             <div className="flex flex-wrap justify-center gap-6">
               {isLoaded === null && (
                 <>
-                  <div className="flex flex-col justify-center items-center text-center absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                    <h1 className="text-red-900 text-2xl mb-1.5">Chyba 404</h1>
-                    <h1 className="text-red-900">Produkty nebyly nalezeny</h1>
-                  </div>
+                  <NotFound link="/view-products" content="Zobrazit produkty" />
                 </>
               )}
               {isLoaded !== null &&
