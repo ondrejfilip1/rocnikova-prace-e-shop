2e3cb6eeac06645fbf1e1423c6cf6e03a8dc1878
ondrejfilip1 <ondrej.filip@student.spsmb.cz>
2025-04-24 19:06:44 +0200

    TYPO

--- server/routes/mail.js
+++ server/routes/mail.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const emailRouter = require("../controllers/mail");
 
 // posle newsletter skupine prijemcu
-// (potrebuje heslo, potebuje emailHTML, potrebuje emailList (pole prijemcu))
+// (potrebuje heslo, potrebuje emailHTML, potrebuje emailList (pole prijemcu))
 router.post("/group", emailRouter.sendGroupNewsletter);
 
 // prida email do databaze
